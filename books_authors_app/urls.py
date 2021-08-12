790718789210e153c14fa91ca27df489aead21dd
jose <jose.hermosillaa@usach.cl>
2021-08-12 19:51:54 -0400

    se termina lo escencial del proyecto

--- books_authors_app/urls.py
+++ books_authors_app/urls.py
@@ -3,5 +3,12 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('books',views.index),
-    path('books/create',views.create_book)
+    path('books/create',views.create_book),
+    path('books/<int:book_id>',views.show_book),
+    path('books/<int:book_id>/assign',views.assign_book),
+    path('authors/<int:author_id>/assign',views.assign_author),
+    path('authors',views.authors),
+    path('authors/create',views.create_author),
+    path('authors/<int:author_id>',views.show_author), 
 ]
+#quizas falta el para eliminar
--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -11,5 +11,49 @@ def create_book(request):
     Book.objects.create(title=request.POST['title'],desc=request.POST['desc'])
     return redirect('/books')
 
+def show_book(request,book_id):
+    book = Book.objects.get(id = book_id)
+    context = {
+        'book':book,
+        'authors': Author.objects.exclude(books__id = book_id)
+    }
+
+    return render(request, 'book.html',context)
+
+def assign_book(request,book_id):
+    book = Book.objects.get(id = book_id)
+    author = Author.objects.get(id =request.POST['author_id'])
+    book.authors.add(author)
+    return redirect(f'/books/{book_id}')
+
+def assign_author(request, author_id):
+    book = Book.objects.get(id = request.POST['book_id'])
+    author = Author.objects.get(id = author_id)
+    book.authors.add(author)
+    return redirect((f'/authors/{author_id}'))
+
+def authors(request):
+    context = { 
+        'authors':Author.objects.all()
+    }
+    return render(request,'authors.html',context)
+
+def create_author(request):
+    Author.objects.create(first_name=request.POST['first_name'],last_name= request.POST['last_name'],notas=request.POST['notas'])
+    return redirect('/authors')
+
+def show_author(request, author_id):
+    author = Author.objects.get(id = author_id)
+    context = {
+        'author': Author.objects.get(id = author_id),
+        'books': Book.objects.exclude(authors__id = author_id)
+    }
+
+    return render(request, 'author.html', context)
+
+
+
+
+
 
 
