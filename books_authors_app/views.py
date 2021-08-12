dcd569c7bfc9313c821b5be85c80662e7be76656
Jose Hermosilla <jose.hermosillaasach.cl>
2021-08-11 22:11:57 -0400

    se agerega url create books

--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 
 def index(request):
@@ -6,6 +6,10 @@ def index(request):
         'books': Book.objects.all()
     }
     return render(request, 'books.html', context)
+
+def create_book(request):
+    Book.objects.create(title=request.POST['title'],desc=request.POST['desc'])
+    return redirect('/books')
 
 
 
